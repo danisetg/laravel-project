451150da434d48e57e1f3d6d4f7368c47f981acf
daniveloper <develored@gmail.com>
2018-06-08 18:06:51 +0200

    installed artisan:passport for oauth2 token security

---
+++ app/Http/Controllers/SenalizacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Senalizacion;
+use Illuminate\Http\Request;
+
+class SenalizacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Senalizacion  $senalizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Senalizacion $senalizacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Senalizacion  $senalizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Senalizacion $senalizacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Senalizacion  $senalizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Senalizacion $senalizacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Senalizacion  $senalizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Senalizacion $senalizacion)
+    {
+        //
+    }
+}
